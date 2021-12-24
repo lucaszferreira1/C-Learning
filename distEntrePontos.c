1cbdce91d9be2ca3763965378b5791a6901dc000
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2021-12-23 21:59:56 -0300

    Update distEntrePontos.c

--- distEntrePontos.c
+++ distEntrePontos.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdio.h>
+#include <string.h>
 
 double distEntrePontos(int x1, int y1, int x2, int y2){
     return (sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2)));
@@ -18,19 +18,19 @@ double distEntrePontoReta(int a, int b, int c, int x, int y){
 int main()
 {
     int x1, y1, x2, y2, z;
-    char escolha;
-    scanf("%c", &escolha);
+    char escolha[1];
+    scanf("%s", escolha);
     scanf("%d", &x1);
     scanf("%d", &y1);
-    scanf("%c", &escolha);
+    scanf("%s", escolha);
     scanf("%d", &x2);
     scanf("%d", &y2);
-    if (escolha == 'R'){
+    if (escolha[0] == 'R'){
         scanf("%d", &z);
         printf("%.2f", distEntrePontoReta(x2, y2, z, x1, y1));
     }
     else{
-        printf("%.1f", distEntrePontos(x1, y1, x2, y2));
+        printf("%.2f", distEntrePontos(x1, y1, x2, y2));
     }
     
     return 0;
