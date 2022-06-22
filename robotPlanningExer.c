ea8b3936a94fedbb82e3a5dc4124acab6a774016
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2022-06-22 16:53:56 -0300

    Update robotPlanningExer.c

--- robotPlanningExer.c
+++ robotPlanningExer.c
@@ -6,11 +6,26 @@ int tamanhos[] = {1, 1, 1, 1, 1};
 void mostra(int **vp){
     int i, j;
     for (i=0;i<5;i++){
-        for (j=0;j<3;j++){
-            if (tamanhos[i] != 0)
-                printf("[%d]", vp[i][j]);
+        for (j=0;j<5;j++){
+            if (tamanhos[i] > 0)
+                printf("[%d]", ((vp[i][j] > 0) ? vp[i][j]-1 : 0));
         }
         printf("\n");
+    }
+}
+
+void find(int **vp, int n, int* vallis, int* valin){ 
+    int i, j;
+    for (i=0;i<5;i++){
+        for (j=0;j<5;j++){
+            if (tamanhos[i] != 0){
+                if (n == vp[i][j]){
+                    *vallis = i;
+                    *valin = j;
+                    return;
+                }
+            }
+        }
     }
 }
 
@@ -23,6 +38,24 @@ void moveto(int **vp, int a, int b){
     vp[a] = realloc(vp[a], sizeof(int) * tamanhos[a]);
 }
 
+void moveon(int **vp, int a, int b){
+    int posa[2], posb[2];
+    int temp = a;
+    find(vp, a, &posa[0], &posa[1]);
+    find(vp, b, &posb[0], &posb[1]);
+    int i;
+    for (i=posa[1];i<5;i++){
+        if (vp[posa[0]][i+1])
+            vp[posa[0]][i] = vp[posa[0]][i+1];
+        else
+            vp[posa[0]][i] = NULL;
+    }
+    for (i=posb[1];i<5;i++){
+        vp[posb[0]][i+1] = temp;
+        temp = vp[posb[0]][i+2];
+    }
+}
+
 int main()
 {
     int *vp[5];
@@ -31,13 +64,12 @@ int main()
 		vp[i] = malloc(sizeof(int));
 	}
 	for (i=0;i<5;i++){
-		vp[i][0] = i;
+		vp[i][0] = i+1;
 	}
-	moveto(vp, 0, 3);
-	moveto(vp, 1, 4);
-	moveto(vp, 2, 3);
+// 	moveto(vp, 1, 2);
+// 	moveto(vp, 0, 4);
 	moveto(vp, 3, 4);
-// 	moveto(vp, 4, 3);
+// 	moveon(vp, 4, 3);
 	mostra(vp);
     return 0;
 }
