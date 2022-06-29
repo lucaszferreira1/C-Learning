da1ce59bd534c45aedd5f24cc0ec52d7c6a6fdd2
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2022-06-29 17:14:19 -0300

    Update Trabalho4Prog.c

--- Trabalho4Prog.c
+++ Trabalho4Prog.c
@@ -3,34 +3,33 @@
 
 int main(){
 	int i, j;
-  int lista[10];
+  int mazer, mezer;
+  int *lista[9];
   int listSizes[9];
 	FILE *entrada = fopen("samples.txt", "r");
 	FILE *saida = fopen("results.txt", "w+");
 
-  fscanf(entrada, "%d", listSizes);
-  printf("%d ", listSizes[0]);
-  // for (i=1;i<9;i++){
-    
-  // }
   
-  for (i=0;i<10;i++){
-    fscanf(entrada, "%d", lista+i);
+  for (i=0;i<9;i++){
+    fscanf(entrada, "%d", &listSizes[i]);
+    lista[i] = malloc(sizeof(int) * listSizes[i]);
+    for (j=0;j<listSizes[i];j++){
+      fscanf(entrada, "%d", &lista[i][j]);
+    }
   }
   
-	for (i=0;i<1;i++){
-		int mazer = 0;
-    int mezer = 0;
-		for (j=1;j<lista[0]+1;j++){
-      if (lista[j] < 0){
-        mezer++;
-      }else{
+	for (i=0;i<9;i++){
+		mazer = 0;
+    mezer = 0;
+		for (j=0;j<listSizes[i];j++){
+      if (lista[i][j]>0){
         mazer++;
+      }else{
+        mezer++;
       }
     }
-    printf("%d Negativos, %d Positivos", mezer, mazer);
+    fprintf(saida, "%d Negativos, %d Positivos\n", mezer, mazer);
 	}
-    
 	
 	fclose(entrada);
   fclose(saida);
