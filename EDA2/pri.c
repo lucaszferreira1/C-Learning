428b94eab06625d327ebcc57364051cfa1cace63
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2023-03-16 18:08:27 -0300

    Update pri.c

--- EDA2/pri.c
+++ EDA2/pri.c
@@ -4,14 +4,16 @@ se a soma de qualquer par de números em v for igual a k.*/
 
 int main()
 {
-    int v[] = {1,2,3,4,5};
-    int k = 9;
+    int v[] = {10,15,3,7};
+    int k = 17;
     for (int i = 0;i<(sizeof(v) / sizeof(int));i++){
         for (int j = i+1;j < (sizeof(v) / sizeof(int));j++){
             if (v[i]+v[j] == k){
+                printf("True");
                 return 0;
             }
         }
     }
+    printf("False");
     return 1;
 }
