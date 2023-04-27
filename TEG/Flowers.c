ce208c5de993f5a32d4a466ae6ba78beb76e18ba
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2023-04-27 11:46:09 -0300

    Create Flowers.c

--- TEG/Flowers.c
+++ TEG/Flowers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MAXCHAR 128
 
@@ -9,24 +10,87 @@ typedef struct{
     char nome[16];
 }Flor;
 
+
 int main()
 {
     FILE *fp;
-    char line[MAXCHAR];
-    fp = fopen("flores.txt", "r");
-    
+    Flor flores[150];
+    fp = fopen("flowers.txt", "r");
+    // O arquivo é aberto e se lê o conteúdo, o qual vai para a lista flores
     if (!fp)
         printf("Can't open file\n");
     else{
-        float valFl;
-        char name[16];
+        Flor flor;
+        int cont = 0;
+        char line[MAXCHAR];
+        char *token;
         while(fgets(line, MAXCHAR, fp )!= NULL){
-            strcpy(name, strtok(line, ","));
-            printf("%.2f\n", atof(name));
+            token = strtok(line, ",");
+            flor.sepLen = atof(token);
+            token = strtok(NULL, ",");
+            flor.sepWid = atof(token);
+            token = strtok(NULL, ",");
+            flor.petLen = atof(token);
+            token = strtok(NULL, ",");
+            flor.petWid = atof(token);
+            token = strtok(NULL, ",");
+            strcpy(flor.nome, token);
+            
+            flores[cont] = flor;
+            cont++;
         }
+        
     }
     
     fclose(fp);
+    
+    // Cálculo de distância euclideana
+    if (flores){
+        Flor pri, sec;
+        int matriz[150][150];
+        // Todos os valores são inicializados e setados para 0
+        for (int i = 0;i < 150;i++){
+            for (int j = 0;j < 150;j++){
+                matriz[i][j] = 0;
+            }
+        }
+        // É calculado a distância euclideana de todos para todos
+        for (int i = 0;i < 150;i++){
+            for (int j = 0;j < 150;j++){
+                pri = flores[i];
+                sec = flores[j];
+                if (i != j){
+                    float eucDist = pow((pri.sepLen - sec.sepLen), 2) + pow((pri.sepWid - sec.sepWid), 2) + pow((pri.petLen - sec.petLen), 2) + pow((pri.petWid - sec.petWid), 2);
+                    if (eucDist <= .3){
+                        matriz[i][j] = 1;
+                        matriz[j][i] = 1;
+                    }
+                }
+            }
+        }
+        
+        // É aberto o arquivo com os resultados
+        fp = fopen("results.txt", "w");
+        if (!fp){
+            printf("Can't open file\n");
+        }else{
+            // É adicionado os resultados ao arquivo
+            char str[150];
+            fprintf(fp, "150\n");
+            for (int i = 0;i < 150;i++){
+                for (int j = 0;j < 150;j++){
+                    // char c = matriz[i][j] + '0';
+                    // str[j] = c;
+                    if (matriz[i][j] == 1){
+                        fprintf(fp, "%d %d\n", i, j);
+                    }
+                }
+                // fprintf(fp, "%s\n", str);
+            }
+        }
+    }
+    
+    
     
     return 0;
 }
