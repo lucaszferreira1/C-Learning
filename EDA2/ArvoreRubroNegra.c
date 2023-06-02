ba11f33bc6050ebf84f7ed436b1a786b740e4f88
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2023-06-02 16:22:01 -0300

    Update ArvoreRubroNegra.c

--- EDA2/ArvoreRubroNegra.c
+++ EDA2/ArvoreRubroNegra.c
@@ -1,182 +1,210 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <stdio.h>
 
 #define MAX 1
 
-enum coloracao { Preto, Vermelho };
+int n_operacoes = 0;
 
+enum coloracao { Vermelho, Preto };
 typedef enum coloracao Cor;
 
 typedef struct no {
     struct no* pai;
     struct no* esquerda;
     struct no* direita;
-    Cor cor; //cor do nó (Vermelho ou Preto)
+    Cor cor;
     int valor;
 } No;
 
-typedef struct arvore{
+typedef struct arvore {
     struct no* raiz;
+    struct no* nulo; 
 } Arvore;
 
+No* criarNo(Arvore*, No*, int);
+void balancear(Arvore*, No*);
 void rotacionarEsquerda(Arvore*, No*);
 void rotacionarDireita(Arvore*, No*);
-void balancear(Arvore*, No*);
+
+Arvore* criar();
+int vazia(Arvore*);
 No* adicionar(Arvore*, int);
-No* adicionarNo(No*, int);
+No* localizar(Arvore* arvore, int valor);
+
+Arvore* criar() {
+    Arvore *arvore = malloc(sizeof(Arvore));
+    arvore->nulo = NULL;
+    arvore->raiz = NULL;
+
+    arvore->nulo = criarNo(arvore, NULL, 0);
+    arvore->nulo->cor = Preto;
+
+    return arvore;
+}
 
 int vazia(Arvore* arvore) {
     return arvore->raiz == NULL;
+}
+
+No* criarNo(Arvore* arvore, No* pai, int valor) {
+    No* no = malloc(sizeof(No));
+
+    no->pai = pai;    
+    no->valor = valor;
+    no->direita = arvore->nulo;
+    no->esquerda = arvore->nulo;
+
+    return no;
+}
+
+No* adicionarNo(Arvore* arvore, No* no, int valor) {
+    if (valor > no->valor) {
+        if (no->direita == arvore->nulo) {
+            no->direita = criarNo(arvore, no, valor);     
+            no->direita->cor = Vermelho;       
+        		
+            return no->direita;
+        } else {
+            return adicionarNo(arvore, no->direita, valor);
+        }
+    } else {
+        if (no->esquerda == arvore->nulo) {
+            no->esquerda = criarNo(arvore, no, valor);
+            no->esquerda->cor = Vermelho;
+            
+            return no->esquerda;
+        } else {
+            return adicionarNo(arvore, no->esquerda, valor);
+        }
+    }
+}
+
+No* adicionar(Arvore* arvore, int valor) {
+    if (vazia(arvore)) {
+        arvore->raiz = criarNo(arvore, arvore->nulo, valor);
+        arvore->raiz->cor = Preto;
+        	
+        return arvore->raiz;
+    } else {
+        No* no = adicionarNo(arvore, arvore->raiz, valor);
+        balancear(arvore, no);
+        
+        return no;
+    }
+}
+
+No* localizar(Arvore* arvore, int valor) {
+    if (!vazia(arvore)) {
+        No* no = arvore->raiz;
+
+        while (no != arvore->nulo) {
+            if (no->valor == valor) {
+                return no;
+            } else {
+                no = valor < no->valor ? no->esquerda : no->direita;
+            }
+        }
+    }
+
+    return NULL;
 }
 
 void visitar(int valor){
     printf("%d ", valor);
 }
 
-Arvore* criar() {
-    Arvore *arvore = malloc(sizeof(Arvore));
-    arvore->raiz = NULL;
-  
-    return arvore;
-}
-
-No* adicionar(Arvore* arvore, int valor) {
-    if (vazia(arvore)) {
-        printf("Adicionando %d\n",valor);
-        No* novo = malloc(sizeof(No));
-        novo->valor = valor;
-        novo->pai = NULL;
-        novo->esquerda = NULL;
-        novo->direita = NULL;
-        novo->cor = Preto;
-        
-        arvore->raiz = novo;
-			
-        return novo;
-    } else {
-        No* no = adicionarNo(arvore->raiz, valor);
-        balancear(arvore, no);
-        printf("Adicionando %d\n",valor);
-        return no;
-    }
-}
-
-No* adicionarNo(No* no, int valor) {
-    if (valor > no->valor) {
-        if (no->direita == NULL) {
-            No* novo = malloc(sizeof(No));
-            novo->valor = valor;
-            novo->pai = no;
-            novo->esquerda = NULL;
-            novo->direita = NULL;
-            novo->cor = Vermelho;
-
-            no->direita = novo;
-				
-            return novo;
-        } else 
-            return adicionarNo(no->direita, valor);
-    } else if (no->esquerda == NULL) {
-            No* novo = malloc(sizeof(No));
-			novo->valor = valor;
-            novo->pai = no;
-            novo->esquerda = NULL;
-            novo->direita = NULL;
-            novo->cor = Vermelho;
-			
-            no->esquerda = novo;
-			
-            return novo;
-        } else 
-            return adicionarNo(no->esquerda, valor);
-}
-
 void balancear(Arvore* arvore, No* no) {
-    while (arvore->raiz != no && no->pai->cor == Vermelho) { //Garante que todos os níveis foram balanceados
+    while (no->pai->cor == Vermelho) {
         if (no->pai == no->pai->pai->esquerda) {
             No *tio = no->pai->pai->direita;
             
-            if (tio != NULL && tio->cor == Vermelho) {
-                tio->cor = Preto; //Resolve caso 2
-                no->pai->cor = Preto;
-                no->pai->pai->cor = Vermelho;
-                no = no->pai->pai; //Vai para o nível anterior
+            if (tio->cor == Vermelho) {
+                tio->cor = Preto; //Caso 1
+                no->pai->cor = Preto; 
+
+                no->pai->pai->cor = Vermelho; //Caso 1
+                no = no->pai->pai; //Caso 1
             } else {
                 if (no == no->pai->direita) {
-                    no = no->pai; //Vai para o nível anterior
-                    rotacionarEsquerda(arvore, no); //Resolve caso 3
+                    no = no->pai; //Caso 2
+                    rotacionarEsquerda(arvore, no); //Caso 2
                 } else {
-                    no->pai->cor = Preto; //Resolve caso 4
-                    no->pai->pai->cor = Vermelho;
-                    rotacionarDireita(arvore, no->pai->pai);
+                    no->pai->cor = Preto; 
+                    no->pai->pai->cor = Vermelho; //Caso 3
+                    rotacionarDireita(arvore, no->pai->pai); //Caso 3
                 }
             }
-        } else if (no->pai == no->pai->pai->direita){
-        //Repete o mesmo código do bloco if, invertendo o lado dos direita e esquerda
+        } else {
             No *tio = no->pai->pai->esquerda;
             
-            if (tio != NULL && tio->cor == Vermelho) {
-                tio->cor = Preto; //Resolve caso 2
-                no->pai->cor = Preto;
-                no->pai->pai->cor = Vermelho;
-                no = no->pai->pai; //Vai para o nível anterior
+            if (tio->cor == Vermelho) {
+                tio->cor = Preto; //Caso 1
+                no->pai->cor = Preto; 
+
+                no->pai->pai->cor = Vermelho; //Caso 1
+                no = no->pai->pai; //Caso 1
             } else {
                 if (no == no->pai->esquerda) {
-                    no = no->pai; //Vai para o nível anterior
-                    rotacionarDireita(arvore, no); //Resolve caso 3
+                    no = no->pai; //Caso 2
+                    rotacionarDireita(arvore, no); //Caso 2
                 } else {
-                    no->pai->cor = Preto; //Resolve caso 4
-                    no->pai->pai->cor = Vermelho;
-                    rotacionarEsquerda(arvore, no->pai->pai);
+                    no->pai->cor = Preto; 
+                    no->pai->pai->cor = Vermelho; //Caso 3
+                    rotacionarEsquerda(arvore, no->pai->pai); //Caso 3
                 }
             }
         }
     }
-    arvore->raiz->cor = Preto; //Resolve caso 1
+    arvore->raiz->cor = Preto; //Conserta possível quebra regra 2
 }
 
 void rotacionarEsquerda(Arvore* arvore, No* no) {
+    n_operacoes++;
     No* direita = no->direita;
-    no->direita = direita->esquerda;
-    if (direita->esquerda != NULL)
-        direita->esquerda->pai = no; //Se houver filho à esquerda em direita, ele será pai do nó
-    direita->pai = no->pai; //Ajusta no pai do nó à direita
+    no->direita = direita->esquerda; 
+
+    if (direita->esquerda != arvore->nulo) {
+        direita->esquerda->pai = no;
+    }
+
+    direita->pai = no->pai;
     
-    if (no->pai == NULL)
-        arvore->raiz = direita; //Se nó for raiz, o nó direita será a nova raiz da árvore
-    else if (no == no->pai->esquerda)
-        no->pai->esquerda = direita; //Corrige relação pai-filho do novo pai (esquerda)
-    else
-        no->pai->direita = direita; //Corrige relação pai-filho do novo pai (direita)
-    direita->esquerda = no; //Corrige relação pai-filho entre o nó pivô e o nó à direita
+    if (no->pai == arvore->nulo) {
+        arvore->raiz = direita;
+    } else if (no == no->pai->esquerda) {
+        no->pai->esquerda = direita;
+    } else {
+        no->pai->direita = direita;
+    }
+
+    direita->esquerda = no;
     no->pai = direita;
 }
 
 void rotacionarDireita(Arvore* arvore, No* no) {
+    n_operacoes++;
     No* esquerda = no->esquerda;
     no->esquerda = esquerda->direita;
-    if (esquerda->direita != NULL)
-        esquerda->direita->pai = no; //Se houver filho à direita em esquerda, ele será pai do nó
-    esquerda->pai = no->pai; //Ajusta no pai do nó à esquerda
     
-    if (no->pai == NULL)
-        arvore->raiz = esquerda; //Se nó for raiz, o nó esquerda será a nova raiz da árvore
-    else if (no == no->pai->esquerda)
-        no->pai->esquerda = esquerda; //Corrige relação pai-filho do novo pai (esquerda)
-    else
-        no->pai->direita = esquerda; //Corrige relação pai-filho do novo pai (direita)
-    esquerda->direita = no; //Corrige relação pai-filho entre o nó pivô e o nó à esquerda
+    if (esquerda->direita != arvore->nulo) {
+        esquerda->direita->pai = no;
+    }
+    
+    esquerda->pai = no->pai;
+    
+    if (no->pai == arvore->nulo) {
+        arvore->raiz = esquerda;
+    } else if (no == no->pai->esquerda) {
+        no->pai->esquerda = esquerda;
+    } else {
+        no->pai->direita = esquerda;
+    }
+    
+    esquerda->direita = no;
     no->pai = esquerda;
 }
 
-void percorrer(No* no, void (*callback)(int)) {
-    if (no != NULL) {
-        percorrer(no->esquerda,callback);
-        callback(no->valor);
-        percorrer(no->direita,callback);
-    }
-}
+
 
 int compara(const void* a, const void* b) {
     return (*(int*) a - *(int*) b);
@@ -196,23 +224,22 @@ int* geraVetor(int n) {
 
 int main()
 {
-    int n = 10000;
-    int* v = geraVetor(n + 1);
-
-    int medio = v[rand() % n];
-    int pior = (n * MAX) + 1;
-    int melhor = v[0];
+    FILE *fp;
+    fp = fopen("results.txt", "w");
+    if (!fp){
+        printf("Can't open file\n");
+    }else{
+        int n = 10000;
+        int* v = geraVetor(n + 1);
     
-    clock_t begin = clock();
-    Arvore* a = criar();
-    for (int i=0;i<n;i++){
-        adicionar(a, v[i]);
+        Arvore* a = criar();
+        for (int i=0;i<n;i++){
+            adicionar(a, v[i]);
+            printf("%d %d\n", i, n_operacoes);
+            fprintf(fp, "%d %d\n", i, n_operacoes);
+        }
     }
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    
-    percorrer(a->raiz, visitar);
-    printf("\n%f", time_spent);
+    return 0;
     
     return 0;
 }
